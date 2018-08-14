da8b5f685c3af11ccf3070a2e881ed769ca4fa2f
Heba-Ahmad-Saada <hebasaada@msn.com>
2018-08-14 17:39:58 +0300

    create loop to allow the player to make up to 5 guesses

--- p1_board-game.py
+++ p1_board-game.py
@@ -14,6 +14,7 @@ for i in range(5):
 def print_board(board):
     for row in board:
         print " ".join(row)
+print_board(board)
 #create 2 variables to store ship's location
 def random_row(board):
     return randint(0,len(board)-1)
@@ -25,3 +26,29 @@ ship_col = random_col(board)
 
 print ship_row
 print ship_col
+#create for loop to allow the player to make up to 5 guesses
+for turn in range(6):
+    print "Turn1", turn +1
+#the code to allow the player to guess where it is:
+    guess_row = int(raw_input("Guess Row: "))
+    guess_col = int(raw_input("Guess Col: "))
+# add conditions to check if guess_row/col equals to ship_row/col
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sank the battleship!"
+        board[guess_row][guess_col]= "$"
+        print_board(board)
+        break
+    else:
+        if guess_row not in range(5) or guess_col not in range(5):
+            print "that's not in the ocean"
+        elif board[guess_row][guess_col] == "X":
+            print "you already guessed that one."
+        else:
+            print "you missed the battleship"
+            board[guess_row][guess_col] = "X"
+        if turn == 5:
+            print "Game OVER"
+            board[ship_row][ship_col]= "$"
+        print_board(board)
+        
+        
