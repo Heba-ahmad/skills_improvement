500680be865ed772441a991e68e9da8cebc91387
Heba-Ahmad-Saada <hebasaada@msn.com>
2018-08-14 15:25:01 +0300

    create variables to store ship's location

--- p1_board-game.py
+++ p1_board-game.py
@@ -1,3 +1,4 @@
+from random import randint
 # In this project I'll build one-player version of the classic board game Battleship!
 # there will be a single ship hidden in a random location on a 5x5 grid.
 # The player will have 10 guesses to try to sink the ship.
@@ -10,6 +11,17 @@ for i in range(5):
     board.append(["*"]* 5)
 #define a function to print the board and getting rid of those
 #quote marks and commas using ".join"
-def print_board(board_in):
+def print_board(board):
     for row in board:
         print " ".join(row)
+#create 2 variables to store ship's location
+def random_row(board):
+    return randint(0,len(board)-1)
+def random_col(board):
+    return randint(0,len(board[0])-1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+print ship_row
+print ship_col
